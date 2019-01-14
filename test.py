0214d310a29f25feae76314768b3d4912296a5c2
Abhi Sinha <sinha45@purdue.edu>
2019-01-13 22:02:06 -0500

    test conncetion/login to reddit

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-#hello
+#!/usr/bin/python3
+import praw
 
-for e in range(90000):
-    print("gay")
+USERNAME = "Adlib_bot"
+
+reddit = praw.Reddit(client_id = "jlxtwwfRpUiYmw",
+                     client_secret = "wwmXY-wJntO6VfpNzVVXQTKv2nw",
+                     user_agent = "test connection of bot",
+                     username = USERNAME,
+                     password = "Preparation123")
+
+if reddit.user.me() != USERNAME:
+    print("unsuccessful login")
+    exit(1)
+else:
+    print("successful login")
+    print(reddit.user.me())
