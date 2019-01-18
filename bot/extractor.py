c7a82cd8db91173bb0c4ed0cdb4b071416036d21
Abhi Sinha <sinha45@purdue.edu>
2019-01-18 00:12:49 -0500

    modified dir struct, extractor reads from config files

--- bot/extractor.py
+++ bot/extractor.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-import praw
+import praw, prawcore
 import configparser
+from helpers import ExtractorHelper
 
 # Extracts information from Reddit
 class Extractor(object):
-
     def __init__(self, **config):
         cfg = configparser.ConfigParser()
+        helper = ExtractorHelper()
         
         BOT_CFG = "../res/bot.ini"
-        # require config file
         if not cfg.read(BOT_CFG):
-            print("Configuration file for bot not found.")
+            print("Configuration file", BOT_CFG, "not found.")
             raise FileNotFoundError(BOT_CFG)
 
+        self.config = {}
+        _keys = ["subfile", "logfile", "client_id", "client_secret",
+                 "user_agent", "username", "password"]
+        for key in _keys:
+            self.config[key] = helper.read_cfg_opt(cfg, "login_cfg", key)    
+       
+        #print(self.config)
+        self.reddit = praw.Reddit(client_id = self.config["client_id"],
+                          client_secret = self.config["client_secret"],
+                          user_agent = self.config["user_agent"],
+                          username = self.config["username"],
+                          password = self.config["password"])
+        try:
+            self.reddit.user.me()
+        except prawcore.exceptions.OAuthException:
+            print("Unsuccessful login")
+            exit()
 
 
-
-        self.reddit = praw.Reddit(client_id = "jlxtwwfRpUiYmw",
-                client_secret = "wwmXY-wJntO6VfpNzVVXQTKv2nw",
-                user_agent = "test connection of bot",
-                username = USERNAME,
-                password = "Preparation123")
-
-
+if __name__ == "__main__":
+    e = Extractor()
