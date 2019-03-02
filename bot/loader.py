9b34e4f07759061aa4e40d79fe476dad3394dcd3
Abhi Sinha <sinha45@purdue.edu>
2019-03-01 21:26:13 -0500

    slight changes to loader

--- bot/loader.py
+++ bot/loader.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import praw, prawcore
-import configparser
+import configparser as cp
 import os
+import praw
+import prawcore
 
 
-# Load information from config files
 class ConfigLoader(object):
+    """
 
+    """
     def __init__(self, loc=None):
-        self.__cfg = configparser.ConfigParser()
+        self.__cfg = cp.ConfigParser()
         
         CFG_PATH = self.__get_cfg_path(loc)
         if not self.__cfg.read(CFG_PATH):
@@ -18,26 +20,9 @@ class ConfigLoader(object):
         self.__keys = ["subfile", "logfile", "client_id", "client_secret",
                  "user_agent", "username", "password"]
 
-    def load_config(self):
-        self.config = {}
-        for key in self.__keys:
-            self.config[key] = self.__read_cfg_opt(self.__cfg, "login_cfg", key)    
-     
-    def spawn_reddit_inst(self, verify=None):
-        self.reddit = praw.Reddit(client_id = self.config["client_id"],
-                              client_secret = self.config["client_secret"],
-                              user_agent = self.config["user_agent"],
-                              username = self.config["username"],
-                              password = self.config["password"])
-        if verify is not None:
-            return (self.reddit, self.__verify_login())
-        else:
-            return self.reddit
-
-    def __get_cfg_path(self, loc=None):
-        if loc is None:
-            loc = "res/bot.ini"
+    def __get_cfg_path(self, loc="res/bot.ini"):
         exec_path = os.path.dirname(os.path.realpath(__file__))
+
         return os.path.join(os.path.split(exec_path)[0], loc)
 
     def __read_cfg_opt(self, cfg_parser, sect, opt):
@@ -52,4 +37,20 @@ class ConfigLoader(object):
             return True
         except prawcore.exceptions.OAuthException:
             return False 
+
+    def load_config(self):
+        self.config = {}
+        for key in self.__keys:
+            self.config[key] = self.__read_cfg_opt(self.__cfg, "login_cfg", key)    
+     
+    def spawn_reddit_inst(self, verify=False):
+        self.reddit = praw.Reddit(client_id = self.config["client_id"],
+                                  client_secret = self.config["client_secret"],
+                                  user_agent = self.config["user_agent"],
+                                  username = self.config["username"],
+                                  password = self.config["password"])
+        if verify:
+            return (self.reddit, self.__verify_login())
+        else:
+            return self.reddit
 
